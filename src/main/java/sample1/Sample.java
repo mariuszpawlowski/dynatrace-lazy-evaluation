c982fb54cd23cdf24c2364690089edcf87144a0d
Mariusz Pawlowski <mariusz.pawlowski@dynatrace.com>
2015-09-25 10:00:03 +0200

    lazy evaluation samples

---
+++ src/main/java/sample1/Sample.java
@@ -0,0 +1,27 @@
+package sample1;
+import java.util.List;
+
+public class Sample {
+
+	private static int LIMIT = 200;
+
+	public static void eagerEvaluate(List<String> symbols) {
+		List<StockInfo> stocks = StockFetcher.fetchStockPrices(symbols);
+
+		System.out.println(stocks.stream().filter(stock -> stock.price > LIMIT).findFirst());
+	}
+
+	public static void lazyEvaluate(List<String> symbols) {
+		System.out.println(StockFetcher.fetchStockPricesLazy(symbols).filter(stock -> stock.price > LIMIT).findFirst());
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Tickers.symbols);
+
+		System.out.println("Eager evaluation, not efficient");
+		Timeit.code(() -> eagerEvaluate(Tickers.symbols));
+
+		System.out.println("Lazy evaluation, more efficient");
+		Timeit.code(() -> lazyEvaluate(Tickers.symbols));
+	}
+}
---
+++ src/main/java/sample1/Tickers.java
@@ -0,0 +1,8 @@
+package sample1;
+import java.util.List;
+import java.util.Arrays;
+
+public class Tickers {
+	public static final List<String> symbols = Arrays.asList("AMD", "HPQ", "IBM", "TXN", "VMW", "XRX", "AAPL", "ADBE", "AMZN", "CRAY", "CSCO", "SNE", "GOOG",
+			"INTC", "INTU", "MSFT", "ORCL", "TIBX", "VRSN", "YHOO");
+}
